fb94345c9a7639c440f22e1eae5dd82c14e9bbb3
Dima <leedmitry8091@gmail.com>
2024-04-26 06:21:13 +1100

    add 2 test to store.test.ts for lists and tags reducer

--- __tests__/redux/store.test.ts
+++ __tests__/redux/store.test.ts
@@ -1,27 +1,53 @@
-import { store } from '@/app/redux/store';
-import { addTodo, deleteTodo } from '@/app/redux/Features/todosSlice';
-import { addSticker, deleteSticker } from '@/app/redux/Features/stickersSlice';
+import { store } from '@/app/redux/store'
+import { addTodo, deleteTodo } from '@/app/redux/Features/todosSlice'
+import { addSticker, deleteSticker } from '@/app/redux/Features/stickersSlice'
+import { addList, deleteList } from "@/app/redux/Features/listsSlice"
+import { addTag, deleteTag } from "@/app/redux/Features/tagsSlice"
 
 describe('store', () => {
     it('should dispatch actions for todosSlice', () => {
-        const todo = { id: 1, title: 'Test Todo', description: null, list: null, dueDate: null, tags: [], subtasks: [], completed: false };
-        store.dispatch(addTodo(todo));
-        let state = store.getState();
-        expect(state.todos.todos).toContainEqual(todo);
+        const todo = { id: 1, title: 'Test Todo', description: null, list: null, dueDate: null, tags: [], subtasks: [], completed: false }
+        store.dispatch(addTodo(todo))
+        let state = store.getState()
+        expect(state.todos.todos).toContainEqual(todo)
 
-        store.dispatch(deleteTodo(todo.id));
-        state = store.getState();
-        expect(state.todos.todos).not.toContainEqual(todo);
-    });
+        store.dispatch(deleteTodo(todo.id))
+        state = store.getState()
+        expect(state.todos.todos).not.toContainEqual(todo)
+    })
 
     it('should dispatch actions for stickersSlice', () => {
-        const sticker = { id: 1, title: 'Test Sticker', description: '' };
-        store.dispatch(addSticker(sticker));
-        let state = store.getState();
-        expect(state.stickers.stickers).toContainEqual(sticker);
+        const sticker = { id: 1, title: 'Test Sticker', description: '' }
+        store.dispatch(addSticker(sticker))
+        let state = store.getState()
+        expect(state.stickers.stickers).toContainEqual(sticker)
 
-        store.dispatch(deleteSticker(sticker.id));
-        state = store.getState();
-        expect(state.stickers.stickers).not.toContainEqual(sticker);
-    });
-});
+        store.dispatch(deleteSticker(sticker.id))
+        state = store.getState()
+        expect(state.stickers.stickers).not.toContainEqual(sticker)
+    })
+
+    it('should dispatch actions for listSlice', () => {
+        const newList = { id: 1, name: 'Test List' }
+
+        store.dispatch(addList(newList))
+        let state = store.getState()
+        expect(state.lists.lists).toContainEqual(newList)
+
+        store.dispatch(deleteList(newList.id))
+        state = store.getState()
+        expect(state.lists.lists).not.toContainEqual(newList)
+    })
+
+    it('should dispatch actions for tagSlice', () => {
+        const newTag = { id: 1, name: 'Test Tag' }
+
+        store.dispatch(addTag(newTag))
+        let state = store.getState()
+        expect(state.tags.tags).toContainEqual(newTag)
+
+        store.dispatch(deleteTag(newTag.id))
+        state = store.getState()
+        expect(state.tags.tags).not.toContainEqual(newTag)
+    })
+})
