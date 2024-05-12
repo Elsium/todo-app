3f5783ace3080921acbff0a4ec8b13ba4ebba734
Dima <leedmitry8091@gmail.com>
2024-05-12 17:15:18 +1100

    fix lint jest config, fix tsconfig

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,14 @@
-import type { Config } from '@jest/types';
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-};
+    moduleNameMapper: {
+        ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+}
 
-export default config;
+export default config
