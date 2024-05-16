f6a595e6fec0eed9b59d1d2e504862a529278818
Dima <leedmitry8091@gmail.com>
2024-05-16 18:05:06 +1100

    refactor to lending main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link'
+import React from 'react'
+import Welcome from '@/components/Home/Welcome/Welcome'
+
 export default function Home() {
     return (
-        <main className='p-[40px] min-h-screen flex justify-center font-jost'>
-            Hello World!
-            go to <Link href={`/work`} className='mx-1 text-blue-300 font-bold'>Work</Link> or <Link href={`/login`} className='mx-1 text-blue-300 font-bold'>Login</Link> pages
-        </main>
+        <Welcome/>
     )
 }
