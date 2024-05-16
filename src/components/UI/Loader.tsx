a15b352e2119c0a1dcc4a538821f259202c574cb
Dima <leedmitry8091@gmail.com>
2024-05-16 17:44:51 +1100

    refactor loader

--- src/components/UI/Loader.tsx
+++ src/components/UI/Loader.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import {CircularProgress} from '@mui/material'
+import {Backdrop, CircularProgress} from '@mui/material'
 
 const Loader = () => {
     return (
         <div className='flex items-center justify-center w-full h-screen'>
-            <CircularProgress />
+            <Backdrop
+                sx={{
+                    color: '#fff',
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                }}
+                open={true}
+            >
+                <CircularProgress size={80} thickness={4}/>
+            </Backdrop>
         </div>
     )
 }
