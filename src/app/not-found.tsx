d461be4ee6a8165f6719269494742c7945ba1fed
Dima <leedmitry8091@gmail.com>
2024-05-16 18:04:51 +1100

    grammar refactor

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ import {useRouter} from 'next/navigation'
 const NotFound = () => {
     const route = useRouter()
     return (
-        <section className='w-full h-screen flex items-center justify-center'>
+        <section className='w-full h-screen flex items-center justify-center select-none'>
             <div className='flex items-center justify-between w-[65%]'>
                 <div className='w-[40%]'>
                     <Image src={img} alt='not found'/>
@@ -16,7 +16,7 @@ const NotFound = () => {
                 <div className='w-[3px] h-[200px] bg-listHover rounded'/>
                 <div className='w-[40%] font-poppins'>
                     <p>Please dont change the URL, it possibly to destroy the app.</p>
-                    <p>Or delete your data ooops..</p>
+                    <p>Or delete your data oops..</p>
                     <button onClick={() => route.back()} className='font-jost text-xl text-blue-300 mt-[20px]'>Go back</button>
                 </div>
             </div>
